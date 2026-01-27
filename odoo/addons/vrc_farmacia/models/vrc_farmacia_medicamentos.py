87b61a17fbd93b83d65aaf205f1759e244236f49
Victor <xvictormariox@gmail.com>
2026-01-27 18:17:04 +0100

    Commit agregando vista kanban de medicamentos e imagenes en ella

--- odoo/addons/vrc_farmacia/models/vrc_farmacia_medicamentos.py
+++ odoo/addons/vrc_farmacia/models/vrc_farmacia_medicamentos.py
@@ -7,6 +7,7 @@ class Medicamento(models.Model):
     name = fields.Char(string="Nombre", required=True, help="Introduce el nombre del medicamento")
     precio = fields.Float(string="Precio")
     stock = fields.Integer(string="Stock")
+    imagen = fields.Image('imagen', max_width= 1920 , max_height= 1080)
 
     imagen = fields.Binary(string="Imagen del Medicamento")
     proveedor_id = fields.Many2one(
